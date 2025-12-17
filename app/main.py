40f0bd8dfad8aa1a2d559e7fc735ae5142c80600
Himanshu <yes.himanshu07@gmail.com>
2025-12-17 20:56:02 +0530

    The exit problem is resolved

--- app/main.py
+++ app/main.py
@@ -5,9 +5,14 @@ def main():
     sys.stdout.write("$ ")
     command = input()
     while command != "exit":
-        sys.stdout.write(f"{command}: command not found\n")
         sys.stdout.write("$ ")
         command = input()
+        commands = command.split()
+        if command[0] == "echo" :
+            print(" ".join(commands[1:]))
+        else :
+            print(f"{command}: command not found")
+        
 
 
 
