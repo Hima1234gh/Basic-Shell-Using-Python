0361a7592db9e5472ffe64795adf779fc6f39eb5
Himanshu <yes.himanshu07@gmail.com>
2025-12-17 20:59:50 +0530

    The echo problem resovled

--- app/main.py
+++ app/main.py
@@ -3,18 +3,26 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    command = input()
-    while command != "exit":
-        sys.stdout.write("$ ")
-        command = input()
-        commands = command.split()
-        if command[0] == "echo" :
-            print(" ".join(commands[1:]))
-        else :
-            print(f"{command}: command not found")
+    while True:
+            try:
+                sys.stdout.write("$ ")
+                sys.stdout.flush()
+                command = input()
+            except EOFError:
+                break
+
+            if command.strip() == "exit":
+                break
+
+            if not command.strip():
+                continue
+
+            commands = command.split()
+            if commands[0] == "echo":
+                print(" ".join(commands[1:]))
+            else:
+                print(f"{commands[0]}: command not found")
         
-
-
 
 if __name__ == "__main__":
     main()
